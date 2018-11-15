300867fc808f869b6f671bbe9d00023cc30be34b
Vedant1202 <Vedant1202@users.noreply.github.com>
2018-11-15 22:20:16 +0530

    Benchmarking

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const express = require('express');
-const app     = express();
+const cluster = require('cluster');
 
-function doWork(duration) {
-  const start = Date.now();
-  while((Date.now() - start) < duration) {
-    //do nothing!
+if (cluster.isMaster) {
+  // If cluster is in master mode, then the file will be executed again
+  // in child mode.
+  cluster.fork();
+} else {
+  // Execute it as a child mode. Child mode is nothing but a server instance.
+
+  const express = require('express');
+  const app     = express();
+
+  function doWork(duration) {
+    const start = Date.now();
+    while((Date.now() - start) < duration) {
+      //do nothing!
+    }
   }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there!');
+  });
+
+  app.listen(1500, () => {
+    console.log('App has started on port 1500');
+  });
+
 }
-
-app.get('/', (req, res) => {
-  doWork(5000);
-  res.send('Hi there!');
-});
-
-app.listen(1500, () => {
-  console.log('App has started on port 1500');
-});
