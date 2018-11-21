212302c0dcf624d0cfde9b72607df04795e342a2
Vedant Nandoskar <vedant.nandoskar@gmail.com>
2018-11-21 11:57:08 -0800

    Clustering threads

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const cluster = require('cluster');
+// const cluster = require('cluster');
+// process.env.UV_THREADPOOL_SIZE = 1;
 
-if (cluster.isMaster) {
-  // If cluster is in master mode, then the file will be executed again
-  // in child mode.
-  cluster.fork();
-} else {
+// if (cluster.isMaster) {
+//   // If cluster is in master mode, then the file will be executed again
+//   // in child mode.
+//
+//     cluster.fork();
+//     cluster.fork();
+//
+// } else {
   // Execute it as a child mode. Child mode is nothing but a server instance.
 
   const express = require('express');
   const app     = express();
+  const crypto = require('crypto');
 
   function doWork(duration) {
     const start = Date.now();
@@ -18,8 +23,12 @@ if (cluster.isMaster) {
   }
 
   app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there!');
+    // doWork(5000);
+    const start = Date.now();
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      console.log('The hash function took ' + (Date.now() - start) + ' ms.');
+      res.send('Hi there!');
+    });
   });
 
   app.get('/dash', (req, res) => {
@@ -30,4 +39,4 @@ if (cluster.isMaster) {
     console.log('App has started on port 1500');
   });
 
-}
+// }
